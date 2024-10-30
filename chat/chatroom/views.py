e7536b11045efbebb77441b93a4fdcc197254ec2
Missbellum <enjaykanz@gmail.com>
2024-10-30 19:23:52 +0100

    Changed AI model from OpenAI to Gemini

--- chat/chatroom/views.py
+++ chat/chatroom/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from openai import OpenAI
-from .models import TextBot
+import google.generativeai as genai
+from dotenv import load_dotenv
+# from .models import TextBot
 import os
 
+load_dotenv()
 
 # Create your views here.
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Write a story about a magic backpack.")
+print(response.text)
 
 def home(request):
     return render(request, 'home.html')
@@ -14,12 +20,12 @@ def home(request):
 def chatroom(request):
     return render(request, 'chatroom.html')
 
-def textbot(request):
-    if request.method == 'POST':
-        user_input = request.POST.get('userInput')  
-        completion = client.chat.completions.create(model='gpt-4o', messages=[{'role': 'user', 'content': user_input}])
-        message = completion.choices[0].message
-        return render(request, 'chatpage.html', context={'message': message})
+# def textbot(request):
+#     if request.method == 'POST':
+#         user_input = request.POST.get('userInput')  
+#         completion = client.chat.completions.create(model='gpt-4o', messages=[{'role': 'user', 'content': user_input}])
+#         message = completion.choices[0].message
+#         return render(request, 'chatpage.html', context={'message': message})
 
     
 
